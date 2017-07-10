f5186988d9aa5bf19a291738e0aeb228cce93f9f
Thomas Glaeser <thomas@webmethods.com>
2017-07-10 16:38:59 -0400

    Normalizing integration test

--- tests/protocol/src/integTest/groovy/com/softwareag/tom/integration/protocol/BurrowTest.groovy
+++ tests/protocol/src/integTest/groovy/com/softwareag/tom/integration/protocol/BurrowTest.groovy
@@ -10,6 +10,8 @@ import com.softwareag.tom.extension.Node
 import groovyx.net.http.ContentType
 import groovyx.net.http.HttpResponseDecorator
 import groovyx.net.http.RESTClient
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -19,6 +21,8 @@ import spock.lang.Specification
  */
 class BurrowTest extends Specification {
 
+    protected static final Logger logger = LoggerFactory.getLogger(BurrowTest.class);
+
     @Shared @Node protected ConfigObject config
     @Shared protected RESTClient client
 
@@ -27,50 +31,69 @@ class BurrowTest extends Specification {
         client = new RESTClient("http://${config.node.host.ip}:${config.node.host.port}")
     }
 
-    public "test 'getClientVersion' via http"() {
-        when: 'we make a get request'
-        HttpResponseDecorator resp = client.get(path: '/network/client_version', contentType: ContentType.JSON.toString()) as HttpResponseDecorator
+    def "test 'getClientVersion' via http"() {
+        given: 'a valid HTTP request'
+        def request = '/network/client_version'
+
+        when: 'the request is send'
+        HttpResponseDecorator resp = send request
 
         then: 'we receive a valid response'
-        resp.success
-        resp.status == 200
-        resp.contentType == ContentType.TEXT.toString()
-        println "response payload - $resp.data"
         resp.data == ['client_version': '0.8.0']
     }
 
-    public "test 'getClientVersion' via rpc"() {
+    def "test 'getClientVersion' via rpc"() {
         given: 'a valid JSON-RPC request'
-        def requestParams = ['jsonrpc': '2.0', 'id': '1', 'method': 'burrow.getClientVersion']
+        def request = ['jsonrpc': '2.0', 'id': '1', 'method': 'burrow.getClientVersion']
 
-        when: 'we post the request'
-        HttpResponseDecorator resp = client.post(path: '/rpc', contentType: ContentType.JSON.toString(), body: requestParams) as HttpResponseDecorator
+        when: 'the request is send'
+        HttpResponseDecorator resp = send request
 
         then: 'we receive a valid response'
-        resp.success
-        resp.status == 200
-        resp.contentType == ContentType.TEXT.toString()
-        println "response payload - $resp.data"
         resp.data.result.client_version == '0.8.0'
     }
 
-    public "test 'getAccount' via rpc"() {
+    def "test 'getAccount' via rpc"() {
         given: 'a valid JSON-RPC request'
-        def requestParams = ['jsonrpc': '2.0', 'id': '1', 'method': 'burrow.getAccount', 'params': ['address':'E9B5D87313356465FAE33C406CE2C2979DE60BCB']]
+        def request = ['jsonrpc': '2.0', 'id': '1', 'method': 'burrow.getAccount', 'params': ['address':'E9B5D87313356465FAE33C406CE2C2979DE60BCB']]
 
-        when: 'we post the request'
-        HttpResponseDecorator resp = client.post(path: '/rpc', contentType: ContentType.JSON.toString(), body: requestParams) as HttpResponseDecorator
+        when: 'the request is send'
+        HttpResponseDecorator resp = send request
 
         then: 'we receive a valid response'
-        resp.success
-        resp.status == 200
-        resp.contentType == ContentType.TEXT.toString()
-        println "response payload - $resp.data"
         resp.data.result.address == 'E9B5D87313356465FAE33C406CE2C2979DE60BCB'
         resp.data.result.balance == 200000000
         resp.data.result.code == ''
         resp.data.result.pub_key == null
         resp.data.result.sequence == 0
         resp.data.result.storage_root == ''
+    }
+
+    protected HttpResponseDecorator send(String request) {
+        HttpResponseDecorator resp = null
+        try {
+            resp = client.get(path: request, contentType: ContentType.JSON.toString()) as HttpResponseDecorator
+            assert resp.success
+            assert resp.status == 200
+            assert resp.contentType == ContentType.TEXT.toString()
+            println "response payload - $resp.data"
+        } catch (IOException e) {
+            logger.error("Unable to send the request, got exception: " + e);
+        }
+        resp
+    }
+
+    protected HttpResponseDecorator send(Map requestParams) {
+        HttpResponseDecorator resp = null
+        try {
+            resp = client.post(path: '/rpc', contentType: ContentType.JSON.toString(), body: requestParams) as HttpResponseDecorator
+            assert resp.success
+            assert resp.status == 200
+            assert resp.contentType == ContentType.TEXT.toString()
+            println "response payload - $resp.data"
+        } catch (IOException e) {
+            logger.error("Unable to send the request, got exception: " + e);
+        }
+        resp
     }
 }
