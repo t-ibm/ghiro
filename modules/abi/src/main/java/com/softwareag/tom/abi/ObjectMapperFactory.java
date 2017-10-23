5bf6dafba836416524c441641192e3b9e35af90f
Thomas Glaeser <thomas@webmethods.com>
2017-10-23 14:24:57 -0400

    Adding a fs registry for Solidity contracts ... now taking the binary from the compiled Solidity sources

---
+++ modules/abi/src/main/java/com/softwareag/tom/abi/ObjectMapperFactory.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2017 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its
+ * subsidiaries and/or its affiliates and/or their licensors.
+ * Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in
+ * your License Agreement with Software AG.
+ */
+package com.softwareag.tom.abi;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * ABI object mapper factory.
+ */
+public class ObjectMapperFactory {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    private ObjectMapperFactory() {}
+
+    public static ObjectMapper getObjectMapper() { return objectMapper; }
+}
