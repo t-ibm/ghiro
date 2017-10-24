ebe81d175296a4a24de8cc422b41a97b482b147b
Thomas Glaeser <thomas.glaeser@ibm.com>
2017-10-24 11:39:20 -0400

    Extracting base functionality from class HexValue into class HexValueBase

--- modules/protocol/src/main/java/com/softwareag/tom/protocol/util/HexValue.java
+++ modules/protocol/src/main/java/com/softwareag/tom/protocol/util/HexValue.java
@@ -7,136 +7,33 @@
 package com.softwareag.tom.protocol.util;
 
 import com.google.protobuf.ByteString;
+import com.softwareag.tom.util.HexValueBase;
 
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 
 /**
- * The hex value encoding helper class. See the <a href="https://github.com/ethereum/wiki/wiki/JSON-RPC#hex-value-encoding">JSON-RPC Wiki</a> for more info.
- * Consequently, we use {@link BigInteger} to represent quantities and {@code byte[]} for raw data.
+ * The hex value encoding helper class with added support of {@link ByteString}.
  */
-public final class HexValue {
-
-    private static final String PREFIX = "0x";
+public final class HexValue extends HexValueBase {
 
     private HexValue() {}
 
-    private static boolean containsPrefix(String input) {
-        return input.length() > 1 && input.charAt(0) == '0' && input.charAt(1) == 'x';
-    }
-
-    private static String validate(String value) {
-        if (value == null) {
-            throw new NumberFormatException("Value cannot be null.");
-        } else if (!value.startsWith(PREFIX)) {
-            throw new NumberFormatException("Value must start with prefix " + PREFIX + ".");
-        } else if (value.length() < 3) {
-            throw new NumberFormatException("Value must be 0[xX][0-9a-fA-F]+.");
-        }
-        return value;
-    }
-
-    public static String addPrefix(String input) {
-        if (!containsPrefix(input)) {
-            return PREFIX + input;
-        } else {
-            return input;
-        }
-    }
-
-    public static String stripPrefix(String input) {
-        if (containsPrefix(input)) {
-            return input.substring(2);
-        } else {
-            return input;
-        }
-    }
-
     public static String stripPrefix(ByteString input) {
         return stripPrefix(toString(input));
+    }
+
+    public static byte[] toByteArray(ByteString value) { return toByteArray(value.toStringUtf8()); }
+
+    public static String toString(ByteString value) {
+        return value.toStringUtf8();
     }
 
     public static BigInteger toBigInteger(ByteString value) {
         return toBigInteger(value.toStringUtf8());
     }
 
-    protected static BigInteger toBigInteger(String value) {
-        return new BigInteger(validate(value).substring(2), 16);
-    }
-
-    protected static BigInteger toBigInteger(byte[] value) {
-        return new BigInteger(1, value);
-    }
-
-    public static byte[] toByteArray(ByteString value) { return toByteArray(value.toStringUtf8()); }
-
-    protected static byte[] toByteArray(BigInteger value, int length) {
-        byte[] result = new byte[length];
-        byte[] bytes = value.toByteArray();
-
-        int bytesLength;
-        int srcOffset;
-        if (bytes[0] == 0) {
-            bytesLength = bytes.length - 1;
-            srcOffset = 1;
-        } else {
-            bytesLength = bytes.length;
-            srcOffset = 0;
-        }
-
-        if (bytesLength > length) {
-            throw new RuntimeException("Input is too large to put in byte array of size " + length);
-        }
-
-        int destOffset = length - bytesLength;
-        System.arraycopy(bytes, srcOffset, result, destOffset, bytesLength);
-        return result;
-    }
-
-    protected static byte[] toByteArray(String input) {
-        String cleanInput = stripPrefix(input);
-
-        int len = cleanInput.length();
-
-        if (len == 0) {
-            return new byte[]{};
-        }
-
-        byte[] data;
-        int startIdx;
-        if (len % 2 != 0) {
-            data = new byte[(len / 2) + 1];
-            data[0] = (byte) Character.digit(cleanInput.charAt(0), 16);
-            startIdx = 1;
-        } else {
-            data = new byte[len / 2];
-            startIdx = 0;
-        }
-
-        for (int i = startIdx; i < len; i += 2) {
-            data[(i + 1) / 2] = (byte) ((Character.digit(cleanInput.charAt(i), 16) << 4)
-                + Character.digit(cleanInput.charAt(i + 1), 16));
-        }
-        return data;
-    }
-
-    public static String toString(ByteString value) {
-        return value.toStringUtf8();
-    }
-
-    protected static String toString(BigInteger value) {
-        if (value.signum() != -1) {
-            return PREFIX + value.toString(16);
-        } else {
-            throw new NumberFormatException("Negative values are not supported");
-        }
-    }
-
-    protected static String toString(byte[] bytes) {
-        StringBuilder sb = new StringBuilder(bytes.length * 2);
-        for (byte b : bytes)
-            sb.append(String.format("%02x", b));
-        return PREFIX + sb.toString();
+    public static ByteString toByteString(byte[] bytes) {
+        return ByteString.copyFromUtf8(toString(bytes));
     }
 
     public static ByteString toByteString(String value) { return ByteString.copyFromUtf8(addPrefix(value)); }
@@ -146,12 +43,4 @@ public final class HexValue {
     }
 
     public static ByteString toByteString(long value) { return ByteString.copyFromUtf8(toString(BigInteger.valueOf(value))); }
-
-    public static ByteString toByteString(byte[] bytes) {
-        return ByteString.copyFromUtf8(toString(bytes));
-    }
-
-    protected static String decode(String hexStr) throws UnsupportedEncodingException {
-        return new String(toByteArray(hexStr), "UTF-8").trim();
-    }
 }
