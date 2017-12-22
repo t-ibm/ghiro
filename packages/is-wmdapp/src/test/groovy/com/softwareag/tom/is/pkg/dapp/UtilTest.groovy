832de145e5989325d4d66275c512fa100d71b8e3
Thomas Glaeser <thomas@webmethods.com>
2017-12-22 18:36:43 -0500

    Initial implementation of ns node creation for Solidity contracts

--- packages/is-wmdapp/src/test/groovy/com/softwareag/tom/is/pkg/dapp/UtilTest.groovy
+++ packages/is-wmdapp/src/test/groovy/com/softwareag/tom/is/pkg/dapp/UtilTest.groovy
@@ -20,7 +20,25 @@ class UtilTest extends Specification {
         given: 'the contracts can be retrieved from the contract registry'
         Map<NSName, NSSignature> contracts = Util.create().getFunctions()
 
-        expect: 'to retrieve a populated list of ns nodes'
-        contracts.size() == 7
+        expect: 'to retrieve a populated map of ns nodes'
+        contracts.size() == 6
+
+        when: 'a particular ns node is retrieved'
+        NSSignature nsSignature = contracts[NSName.create('sample.util.Console:uintToBytes')]
+
+        then: 'the signature of this ns node is as expected'
+        nsSignature.input.fields.length == 1
+        nsSignature.input.fields[0].name == 'v'
+        nsSignature.output.fields.length == 1
+        nsSignature.output.fields[0].name == 'ret'
+
+        when: 'a particular ns node is retrieved'
+        nsSignature = contracts[NSName.create('sample.util.Console:log')]
+        nsSignature.input = nsSignature.getInput()
+
+        then: 'the signature of this ns node is as expected'
+        nsSignature.input.fields.length == 1
+        nsSignature.input.fields[0].name == 'x'
+        nsSignature.output.fields.length == 0
     }
 }
