dcdd20e1ab59d669062a8a6e1756e36fa99ef9ee
Thomas Glaeser <thomas@webmethods.com>
2017-12-27 10:37:05 -0500

    Adding load and deploy services per contract

--- packages/is-wmdapp/src/main/java/wm/dapp/Admin.java
+++ packages/is-wmdapp/src/main/java/wm/dapp/Admin.java
@@ -22,15 +22,19 @@ import com.wm.app.b2b.server.FlowSvcImpl;
 import com.wm.app.b2b.server.Package;
 import com.wm.app.b2b.server.PackageManager;
 import com.wm.app.b2b.server.ServiceException;
+import com.wm.app.b2b.server.ns.Namespace;
 import com.wm.app.b2b.ws.codegen.FlowGenUtil;
 import com.wm.app.b2b.ws.ns.NSFacade;
 import com.wm.data.IData;
+import com.wm.data.IDataFactory;
 import com.wm.lang.flow.FlowInvoke;
 import com.wm.lang.ns.NSName;
 import com.wm.lang.ns.NSSignature;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 // --- <<IS-END-IMPORTS>> ---
 
 public final class Admin {
@@ -47,6 +51,7 @@ public final class Admin {
         Package pkg = PackageManager.getPackage("WmDApp");
         System.setProperty(Node.SYSTEM_PROPERTY_TOMCONFNODE, String.valueOf(pkg.getManifest().getProperty("node")));
         Map<NSName,NSSignature> nsNodes;
+        Set<NSName> contractNodes = new HashSet<>();
         try {
             nsNodes = Util.create().getFunctions();
         } catch (IOException e) {
@@ -55,12 +60,31 @@ public final class Admin {
         try {
             for (Map.Entry<NSName,NSSignature> nsNode : nsNodes.entrySet()) {
                 NSName nsName = nsNode.getKey();
+                if (!contractNodes.contains(nsName.getInterfaceNSName())) {
+                    // Remember the contract nodes
+                    contractNodes.add(nsName.getInterfaceNSName());
+                }
                 NSSignature nsSignature = nsNode.getValue();
                 if (!pkg.getStore().getNodePath(nsName).mkdirs()) {
                     DAppLogger.logDebug(DAppMsgBundle.DAPP_SERVICES_MKDIRS, new Object[]{""+nsName});
                 }
                 FlowSvcImpl flowSvcImpl = FlowGenUtil.getFlowSvcImpl(pkg, nsName, nsSignature, "dapp");
                 FlowInvoke flowInvoke = FlowGenUtil.getFlowInvoke("wm.dapp.Contract:call");
+                flowSvcImpl.getFlowRoot().addNode(flowInvoke);
+                NSFacade.saveNewNSNode(flowSvcImpl);
+            }
+            for (NSName contractNode : contractNodes) {
+                // Add the load service
+                NSName nsName = NSName.create(contractNode.toString(), "load");
+                NSSignature nsSignature = NSSignature.create(Namespace.current(), IDataFactory.create());
+                FlowSvcImpl flowSvcImpl = FlowGenUtil.getFlowSvcImpl(pkg, nsName, nsSignature, "dapp");
+                FlowInvoke flowInvoke = FlowGenUtil.getFlowInvoke("wm.dapp.Contract:load");
+                flowSvcImpl.getFlowRoot().addNode(flowInvoke);
+                NSFacade.saveNewNSNode(flowSvcImpl);
+                // Add the deploy service
+                nsName = NSName.create(contractNode.toString(), "deploy");
+                flowSvcImpl = FlowGenUtil.getFlowSvcImpl(pkg, nsName, nsSignature, "dapp");
+                flowInvoke = FlowGenUtil.getFlowInvoke("wm.dapp.Contract:deploy");
                 flowSvcImpl.getFlowRoot().addNode(flowInvoke);
                 NSFacade.saveNewNSNode(flowSvcImpl);
             }
