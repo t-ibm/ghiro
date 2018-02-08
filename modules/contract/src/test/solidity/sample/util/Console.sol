719a6d8320c7f0b1fb7161fa65b7ebb59810e17b
Thomas Glaeser <thomas.glaeser@ibm.com>
2018-02-07 19:19:58 -0500

    Explicitly declaring visibility specifier

--- modules/contract/src/test/solidity/sample/util/Console.sol
+++ modules/contract/src/test/solidity/sample/util/Console.sol
@@ -7,17 +7,17 @@ contract Console {
     /**
      * @notice Triggering logging of this contract's address.
      */
-    function log() constant {
+    function log() public {
         LogAddress(address(this));
     }
     event LogAddress(address);
     /**
      * @notice Triggering logging of an arbitrary unsigned integer value.
      */
-    function log(uint x) constant {
+    function log(uint x) public {
         LogUint(uintToBytes(x));
     }
-    function uintToBytes(uint v) constant returns (bytes32 ret) {
+    function uintToBytes(uint v) public constant returns (bytes32 ret) {
         if (v == 0) {
             ret = '0';
         }
