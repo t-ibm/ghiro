5d8e0642406256ff5cf1993872b726aa3d203fd3
Thomas Glaeser <thomas.glaeser@ibm.com>
2018-02-15 21:38:56 -0500

    Replaced sample Flow service with Java service using example from Zeppelin

---
+++ packages/is-wmdappcontract/src/main/java/sample/zeppelin/SimpleSavingsWallet.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 1996-2006 webMethods, Inc.
+ * Copyright (c) 2007-2018 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or
+ * its subsidiaries and/or its affiliates and/or their licensors.
+ * Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in
+ * your License Agreement with Software AG.
+ */
+package sample.zeppelin;
+// --- <<B2B-START-IMPORTS>> ---
+import com.softwareag.tom.util.HexValueBase;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+import com.wm.data.IData;
+import com.wm.data.IDataCursor;
+import com.wm.data.IDataFactory;
+import com.wm.data.IDataUtil;
+// --- <<B2B-END-IMPORTS>> ---
+
+@SuppressWarnings("unused") public class SimpleSavingsWallet {
+    public static void sendTo(IData pipeline) throws ServiceException {
+        // --- <<B2B-START(sendTo)>> ---
+        // @sigtype java 3.5
+        // [i] field:0:required payee
+        // [i] field:0:required amount
+        IDataCursor cursor = pipeline.getCursor();
+        String payee = IDataUtil.getString(cursor,"payee");
+        String amount = IDataUtil.getString(cursor,"amount");
+        Object[][] input = {
+                { "payee", HexValueBase.toBigInteger(payee) },
+                { "amount", HexValueBase.toBigInteger(amount) },
+        };
+        try {
+            IData output = Service.doInvoke("zeppelin.examples.SimpleSavingsWallet", "sendTo", IDataFactory.create(input));
+            IDataUtil.merge(pipeline, output);
+        } catch (Exception e) {
+            if (e instanceof ServiceException) {
+                throw (ServiceException) e;
+            } else {
+                throw new ServiceException(e);
+            }
+        }
+        // --- <<B2B-END>> ---
+    }
+    // --- <<IS-START-SHARED>> ---
+    // --- <<IS-END-SHARED>> ---
+}
