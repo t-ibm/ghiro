a96ee6a57f6e5ad3ef60e0a8ae63cbef1660bdc8
Thomas Glaeser <thomas.glaeser@ibm.com>
2018-07-26 11:07:47 -0400

    Adding log observable support plus more helper methods ... making this a non-final class for better unit test support

--- packages/is-wmdapp/src/main/java/com/softwareag/tom/is/pkg/dapp/DAppMsgBundle.java
+++ packages/is-wmdapp/src/main/java/com/softwareag/tom/is/pkg/dapp/DAppMsgBundle.java
@@ -13,15 +13,31 @@ public class DAppMsgBundle extends B2BListResourceBundle {
     static final int FAC = 60;//JournalLogger.FAC_DAPP; //TODO
 
     // Keys
-    static final int DAPP_SERVICES_MKDIRS = 1;
-    static final int DAPP_CONTRACT_DEPLOY = 2;
-    static final int DAPP_CONTRACT_CALL = 3;
+    public static final int DAPP_METHOD_START = 1;
+    public static final int DAPP_METHOD_END = 2;
+    static final int DAPP_SERVICES_MKDIRS = 3;
+    static final int DAPP_CONTRACT_DEPLOY = 4;
+    static final int DAPP_CONTRACT_CALL = 5;
+    static final int DAPP_OBSERVABLE_LOG = 6;
+    public static final int DAPP_TRIGGER_WAITFORTHROTTLED = 7;
+
+    public static final int DAPP_ERROR_INIT = 9001;
+    public static final int DAPP_ERROR_NOTIFICATION = 9002;
+    public static final int DAPP_ERROR_PUT = 9003;
 
     // Values
-    private static final Object [][] contents = new Object[][]{
-            {FAC + "." + DAPP_SERVICES_MKDIRS, "Node directory {0} already exists or creation failed."},
-            {FAC + "." + DAPP_CONTRACT_DEPLOY, "Deployed contract {0} at contract address {1}."},
-            {FAC + "." + DAPP_CONTRACT_CALL, "Called contract function {0}#{1} at {2}."},
+    private static final Object[][] contents = new Object[][]{
+        // Log messages
+        {FAC + "." + DAPP_METHOD_START, "Entering method: {0}"},
+        {FAC + "." + DAPP_METHOD_END, "Completing method: (0)"},
+        {FAC + "." + DAPP_CONTRACT_DEPLOY, "Deployed contract {0} at contract address {1}."},
+        {FAC + "." + DAPP_CONTRACT_CALL, "Called contract function {0}#{1} at {2}."},
+        {FAC + "." + DAPP_OBSERVABLE_LOG, "Registered log observable for contract {0} at contract address {1}."},
+        {FAC + "." + DAPP_TRIGGER_WAITFORTHROTTLED, "Trigger {0} waiting for throttled queue capacity to become available."},
+        // Error messages
+        {FAC + "." + DAPP_ERROR_INIT, "Error during initialization: {0}"},
+        {FAC + "." + DAPP_ERROR_NOTIFICATION, "Received error notification from observable: {0}"},
+        {FAC + "." + DAPP_ERROR_PUT, "Unable to add received result to message queue: {0}"},
     };
 
     @Override protected Object[][] getContents() {
