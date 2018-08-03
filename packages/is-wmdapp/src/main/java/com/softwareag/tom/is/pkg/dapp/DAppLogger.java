7d93c8f559672a2a6ebf316db440a5d7f61f23d2
Thomas Glaeser <thomas.glaeser@ibm.com>
2018-08-03 14:54:20 -0400

    Adding new logging method and message

--- packages/is-wmdapp/src/main/java/com/softwareag/tom/is/pkg/dapp/DAppLogger.java
+++ packages/is-wmdapp/src/main/java/com/softwareag/tom/is/pkg/dapp/DAppLogger.java
@@ -19,22 +19,22 @@ public final class DAppLogger {
     public static void logDebug(int messageId, Object[] params) { JournalLogger.logDebugPlus(messageId, DAppMsgBundle.FAC, params); }
 
     // Info
-    public static void logInfo(int messageId) {
-        JournalLogger.logInfo(messageId, DAppMsgBundle.FAC);
-    }
+    public static void logInfo(int messageId) { JournalLogger.logInfo(messageId, DAppMsgBundle.FAC); }
     public static void logInfo(int messageId, Object param) { JournalLogger.logInfo(messageId, DAppMsgBundle.FAC, param); }
     public static void logInfo(int messageId, Object[] params) { JournalLogger.logInfo(messageId, DAppMsgBundle.FAC, params); }
 
     // Warning
-    public static void logWarning(int messageId) {
-        JournalLogger.logWarning(messageId, DAppMsgBundle.FAC);
-    }
+    public static void logWarning(int messageId) { JournalLogger.logWarning(messageId, DAppMsgBundle.FAC); }
     public static void logWarning(int messageId, Object param) { JournalLogger.logWarning(messageId, DAppMsgBundle.FAC, param); }
     public static void logWarning(int messageId, Object[] params) { JournalLogger.logWarning(messageId, DAppMsgBundle.FAC, params); }
 
     // Error
     public static void logError(int messageId, Throwable e) {
         JournalLogger.logError(messageId, DAppMsgBundle.FAC, e);
+        Server.logError(e);
+    }
+    public static void logError(int messageId, Object param, Throwable e) {
+        JournalLogger.logError(messageId, DAppMsgBundle.FAC, param, e);
         Server.logError(e);
     }
     public static void logError(int messageId, Object[] params, Throwable e) {
