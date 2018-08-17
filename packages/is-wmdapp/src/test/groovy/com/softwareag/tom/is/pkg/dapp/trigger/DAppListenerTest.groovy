56cc6bad35b8c00a970a8bb37ddb62824f072af6
Thomas Glaeser <thomas.glaeser@ibm.com>
2018-08-17 13:31:14 -0400

    Moving and impoving the listener unit test case ... switching to the simplified constructor for class DAppExecutionTask

--- packages/is-wmdapp/src/test/groovy/com/softwareag/tom/is/pkg/dapp/trigger/DAppListenerTest.groovy
+++ packages/is-wmdapp/src/test/groovy/com/softwareag/tom/is/pkg/dapp/trigger/DAppListenerTest.groovy
@@ -5,10 +5,11 @@
  * Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in
  * your License Agreement with Software AG.
  */
-package com.softwareag.tom.is.pkg.dapp
+package com.softwareag.tom.is.pkg.dapp.trigger
 
-import com.softwareag.tom.is.pkg.dapp.trigger.DAppListener
+import com.softwareag.tom.is.pkg.dapp.Util
 import com.softwareag.tom.protocol.Web3Service
+import com.softwareag.tom.protocol.abi.Types
 import com.softwareag.tom.protocol.jsonrpc.Request
 import com.softwareag.tom.protocol.jsonrpc.Response
 import com.softwareag.tom.protocol.jsonrpc.ResponseMock
@@ -16,12 +17,15 @@ import com.softwareag.tom.protocol.jsonrpc.Service
 import com.wm.app.b2b.server.PackageManager
 import com.wm.app.b2b.server.ThreadManager
 import com.wm.app.b2b.server.TriggerFactory
+import com.wm.app.b2b.server.dispatcher.AbstractListener
+import com.wm.app.b2b.server.dispatcher.exceptions.CommException
 import com.wm.app.b2b.server.dispatcher.frameworks.DispatcherManager
 import com.wm.app.b2b.server.dispatcher.frameworks.DispatcherManagerHelper
 import com.wm.app.b2b.server.dispatcher.trigger.Trigger
 import com.wm.app.b2b.server.dispatcher.trigger.control.ControlledTriggerSvcThreadPool
 import com.wm.app.b2b.server.dispatcher.trigger.control.TriggerDispatcherStrategy
 import com.wm.app.b2b.server.dispatcher.trigger.control.TriggerOutputControl
+import com.wm.app.b2b.server.dispatcher.wmmessaging.Message
 import com.wm.app.b2b.server.ns.Namespace
 import com.wm.data.IData
 import com.wm.data.IDataFactory
@@ -41,11 +45,13 @@ import spock.lang.Specification
 class DAppListenerTest extends ListenerSpecification {
 
     def "test log filter observable"() {
-        given: 'the listener some time to startup'
-        sleep(10000)
+        given: 'the listener had some time to startup'
+        sleep(1000)
 
         expect: 'the listener was successfully started'
         listener.running
+
+        and: 'some messages were received'
         listener.queueSize > 0
 
         when: 'the listener will be stopped'
@@ -53,16 +59,23 @@ class DAppListenerTest extends ListenerSpecification {
 
         then: 'the listener state changed accordingly'
         listener.stopped
+
+        when: 'the received messages are now getting processed'
+        processMessage()
+
+        then: 'all messages were consumed'
+        listener.queueSize == 0
     }
 }
 
 abstract class ListenerSpecification extends Specification {
-    @Shared Trigger trigger
+    @Shared ControlledTriggerSvcThreadPool threadPool
     @Shared DAppListener listener
+    @Shared String pdtName
 
     def setup() {
         // The test fixture
-        String pdtName = 'sample.SimpleStorage:LogAddress'
+        pdtName = 'sample.SimpleStorage:LogAddress'
         // Create publishable document type
         NSPackage pkg = PackageManager.getPackage("WmDAppContract")
         IData eventDescription = IDataFactory.create((Object[][]) [['brokerEventTypeName', 'MyBrokerEventTypeName'], ['timeToLive', 0]])
@@ -90,15 +103,27 @@ abstract class ListenerSpecification extends Specification {
         IData data = IDataFactory.create((Object[][]) [['messageTypeFilterPair', messageTypeFilterPair]])
         ICondition condition = ConditionFactory.getInstance(ConditionFactory.SIMPLE).create(data)
         ICondition[] conditions = [condition]
-        trigger = TriggerFactory.createTrigger(pkg, NSName.create(pdtName), conditions)
+        Trigger trigger = TriggerFactory.createTrigger(pkg, NSName.create(pdtName), conditions)
         ThreadManager.init()
         TriggerDispatcherStrategy triggerDispatcherStrategy = TriggerDispatcherStrategy.getInstance()
         TriggerOutputControl triggerOutputControl = new TriggerOutputControl(trigger, 1, DispatcherManager.create(DispatcherManagerHelper.CODE_PATH_DEFAULT_NAME))
         triggerDispatcherStrategy.register(triggerOutputControl)
+        // Inject the thread pool size ... we need at least 2 thread to process the incoming messages
+        threadPool = ControlledTriggerSvcThreadPool.getInstance()
+        threadPool.max = 2
+        threadPool.min = 0
         // Run the listener
-        ControlledTriggerSvcThreadPool threadPool = ControlledTriggerSvcThreadPool.getInstance()
         listener = new DAppListenerMock(trigger, threadPool)
         threadPool.runTarget(listener)
+    }
+
+    def processMessage() {
+        while (listener.queueSize > 0) {
+            println "queue size :: $listener.queueSize"
+            Types.FilterLogType consumerEvent = listener.queue.poll()
+            DAppExecutionTask task = new DAppExecutionTaskMock(consumerEvent, pdtName, listener)
+            task.processMessage()
+        }
     }
 }
 
@@ -106,8 +131,19 @@ class DAppListenerMock extends DAppListener {
     DAppListenerMock(Trigger trigger, ControlledTriggerSvcThreadPool threadPool) {
         super(trigger, threadPool)
     }
-
     @Override protected void init() throws Exception {
         // Do not wait until IS is started
+    }
+    @Override protected void initMessageDispatcher() {
+        // Do not initialize the dispatcher; instead we will bypass the dispatcher and process the message directly
+    }
+}
+
+class DAppExecutionTaskMock extends DAppExecutionTask {
+    DAppExecutionTaskMock(Types.FilterLogType consumerEvent, String pdtName, AbstractListener<Types.FilterLogType> listener) throws CommException {
+        super(consumerEvent, pdtName, listener)
+    }
+    @Override protected void setup(Message<Types.FilterLogType> message) {
+        // Do not setup an IS context
     }
 }
