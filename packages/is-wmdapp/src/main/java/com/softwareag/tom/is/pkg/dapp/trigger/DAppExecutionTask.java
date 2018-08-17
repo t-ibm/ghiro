efb7f9d338b4d11f99bbdce426e46bdf457df74c
Thomas Glaeser <thomas@webmethods.com>
2018-08-17 13:36:45 -0400

    Initial implementation of class com.softwareag.tom.is.pkg.dapp.trigger.DAppExecutionTask

--- packages/is-wmdapp/src/main/java/com/softwareag/tom/is/pkg/dapp/trigger/DAppExecutionTask.java
+++ packages/is-wmdapp/src/main/java/com/softwareag/tom/is/pkg/dapp/trigger/DAppExecutionTask.java
@@ -7,18 +7,18 @@
  */
 package com.softwareag.tom.is.pkg.dapp.trigger;
 
+import com.softwareag.tom.is.pkg.dapp.Util;
 import com.softwareag.tom.protocol.abi.Types;
 import com.wm.app.b2b.server.ProtocolInfoIf;
 import com.wm.app.b2b.server.dispatcher.AbstractExecutionTask;
-import com.wm.app.b2b.server.dispatcher.exceptions.CommException;
+import com.wm.app.b2b.server.dispatcher.AbstractListener;
 import com.wm.app.b2b.server.dispatcher.exceptions.MessagingCoderException;
-import com.wm.app.b2b.server.dispatcher.trigger.Trigger;
-import com.wm.app.b2b.server.dispatcher.trigger.control.ControlledTriggerSvcThreadPool;
-import com.wm.app.b2b.server.dispatcher.trigger.control.TriggerOutputControl;
 import com.wm.app.b2b.server.dispatcher.um.trigger.WmMessagingProtocolInfoImpl;
-import com.wm.app.b2b.server.dispatcher.wmmessaging.ConnectionAlias;
 import com.wm.app.b2b.server.dispatcher.wmmessaging.Message;
+import com.wm.lang.ns.NSName;
 import com.wm.msg.IMessage;
+
+import java.io.IOException;
 
 public class DAppExecutionTask extends AbstractExecutionTask<Types.FilterLogType> {
 
@@ -26,16 +26,25 @@ public class DAppExecutionTask extends AbstractExecutionTask<Types.FilterLogType
      * Default constructor. Note, if the trigger has only one publishable document type associated with it we will already
      * know its name, otherwise we look it up.
      */
-    DAppExecutionTask(ConnectionAlias alias, Types.FilterLogType consumerEvent, String pdtName, Trigger trigger, TriggerOutputControl toc, ControlledTriggerSvcThreadPool threadPool, boolean debugTrace) throws CommException {
-        super(alias, consumerEvent, pdtName, trigger, toc, threadPool, debugTrace);
+    DAppExecutionTask(Types.FilterLogType consumerEvent, String pdtName, AbstractListener<Types.FilterLogType> listener) {
+        super(consumerEvent, pdtName, listener);
     }
 
+    @Override protected void processMessage() { super.processMessage(); }
+
     @Override protected IMessage preprocess() throws MessagingCoderException {
-        return null;
+        NSName nsName = NSName.create(_pdtName);
+        try {
+            Message<Types.FilterLogType> msg = Util.instance.decodeLogEvent(nsName, _event);
+            setup(msg);
+            return msg;
+        } catch (IOException e) {
+            throw new MessagingCoderException(e);
+        }
     }
 
     @Override protected ProtocolInfoIf getProtocolInfoIf(Message<Types.FilterLogType> message) {
-        return new WmMessagingProtocolInfoImpl(WmMessagingProtocolInfoImpl.SubProtocol.DES, _alias.getName(), _trigger.getName(), _pdtName, message.getDeliveryCount(), null);
+        return new WmMessagingProtocolInfoImpl(WmMessagingProtocolInfoImpl.SubProtocol.DES, "tbd", _trigger.getName(), _pdtName, message.getDeliveryCount(), null);
     }
 
     @Override protected void postprocess(IMessage msg, boolean processOk, boolean shouldAck) throws Exception {
