c4962493f83f824ac1154861b77f26d0edaadcad
Thomas Glaeser <thomas@webmethods.com>
2018-08-29 13:56:01 -0400

    Improving listener unit test

--- packages/is-wmdapp/src/test/groovy/com/softwareag/tom/is/pkg/dapp/trigger/DAppListenerTest.groovy
+++ packages/is-wmdapp/src/test/groovy/com/softwareag/tom/is/pkg/dapp/trigger/DAppListenerTest.groovy
@@ -14,6 +14,7 @@ import com.softwareag.tom.protocol.jsonrpc.Request
 import com.softwareag.tom.protocol.jsonrpc.Response
 import com.softwareag.tom.protocol.jsonrpc.ResponseMock
 import com.softwareag.tom.protocol.jsonrpc.Service
+import com.wm.app.b2b.server.FlowSvcImpl
 import com.wm.app.b2b.server.PackageManager
 import com.wm.app.b2b.server.ThreadManager
 import com.wm.app.b2b.server.TriggerFactory
@@ -26,15 +27,18 @@ import com.wm.app.b2b.server.dispatcher.trigger.control.ControlledTriggerSvcThre
 import com.wm.app.b2b.server.dispatcher.trigger.control.TriggerDispatcherStrategy
 import com.wm.app.b2b.server.dispatcher.trigger.control.TriggerOutputControl
 import com.wm.app.b2b.server.dispatcher.wmmessaging.Message
+import com.wm.app.b2b.server.invoke.InvokeManager
 import com.wm.app.b2b.server.ns.Namespace
 import com.wm.data.IData
 import com.wm.data.IDataFactory
+import com.wm.lang.ns.EventDescription
 import com.wm.lang.ns.NSName
 import com.wm.lang.ns.NSPackage
 import com.wm.lang.ns.NSRecord
-import com.wm.lang.ns.NSType
+import com.wm.lang.ns.NSRecordUtil
 import com.wm.msg.ConditionFactory
 import com.wm.msg.ICondition
+import com.wm.util.Name
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -74,48 +78,32 @@ abstract class ListenerSpecification extends Specification {
     @Shared String pdtName
 
     def setup() {
-        // The test fixture
-        pdtName = 'sample.SimpleStorage:LogAddress'
-        // Create publishable document type
+        // Test fixture
         NSPackage pkg = PackageManager.getPackage("WmDAppContract")
-        IData eventDescription = IDataFactory.create((Object[][]) [['brokerEventTypeName', 'MyBrokerEventTypeName'], ['timeToLive', 0]])
-        IData record = IDataFactory.create((Object[][]) [['node_nsName', pdtName], ['node_type', NSType.create('record')], ['eventDescription', eventDescription]])
-        NSRecord pdt = NSRecord.createRecord(Namespace.current(), record)
+        pdtName = 'sample.SimpleStorage:LogAddress'
+        String triggerName = 'sample.SimpleStorage:trigger'
+        String serviceName = 'pub.flow:debugLog'
+        // Create publishable document type
+        EventDescription eventDescription = EventDescription.create(Name.create(pdtName), 0, EventDescription.VOLATILE)
+        NSRecord pdt = new NSRecord(Namespace.current(), pdtName, NSRecord.DIM_SCALAR)
+        pdt.setNSName(NSName.create(pdtName))
         pdt.setPackage(pkg)
+        NSRecordUtil.transform(pdt, eventDescription)
         assert pdt.isPublishable()
         Namespace.current().putNode(pdt)
-        // Create needed instances
-        ResponseMock filterResponseMock = new ResponseMock()
-        Service service = Mock(Service)
-        // Handle service layer communications
-        service.send(_ as Request, _ as Class) >> { Request request, Class c ->
-            println ">>> $request"
-            Response response = filterResponseMock.getResponse(request)
-            println "<<< $response"
-            response
-        }
-        // Inject mock service into Util class
-        Util.instance.web3Service = Web3Service.build(service)
-        // Remember the contract address; implies the contract was deployed
-        Util.instance.storeContractAddress(pdt.getNSName(), filterResponseMock.contractAddress)
+        // Create service to invoke
+        FlowSvcImpl service = new FlowSvcImpl(pkg, NSName.create(serviceName), null)
+        Namespace.current().putNode(service)
         // Create trigger
         IData messageTypeFilterPair = IDataFactory.create((Object[][]) [['messageType', pdt.getNSName().toString()], ['filter', 'contractAddress != null']])
         IData data = IDataFactory.create((Object[][]) [['messageTypeFilterPair', messageTypeFilterPair]])
         ICondition condition = ConditionFactory.getInstance(ConditionFactory.SIMPLE).create(data)
+        condition.serviceName = serviceName
         ICondition[] conditions = [condition]
-        Trigger trigger = TriggerFactory.createTrigger(pkg, NSName.create(pdtName), conditions)
-//        TriggerStatTracker triggerStats = new TriggerStatTracker()
-//        Trigger trigger = Mock(Trigger)
-//        trigger.getTriggerStats() >> triggerStats
-//        trigger.setPackage(_ as NSPackage) >> { callRealMethod() }; trigger.getPackage() >> { callRealMethod() }; trigger.setPackage(pkg)
-//        trigger.setNSName(_ as NSName) >> { callRealMethod() }; trigger.getNSName() >> { callRealMethod() }; trigger.setNSName(pdt.getNSName())
-//        trigger.setConditions(_ as ICondition[]) >> { callRealMethod() }; trigger.getConditions() >> { callRealMethod() }; trigger.setConditions(conditions)
-//        trigger.getName() >> { callRealMethod() }
-//        trigger.findUMChannelFilterPairs() >> { callRealMethod() }
-//        trigger.processMessage(_ as IMessage) >> {
-//            // We stop calling the real method now as we are unable to obtain a reference to the invoke manager
-//            callRealMethod()
-//        }
+        Trigger trigger = TriggerFactory.createTrigger(pkg, NSName.create(triggerName), conditions)
+        // Inject mock invoke manager into Trigger
+        trigger.invokeManager = Mock(InvokeManager)
+        // Initialize trigger manager
         ThreadManager.init()
         TriggerDispatcherStrategy triggerDispatcherStrategy = TriggerDispatcherStrategy.getInstance()
         TriggerOutputControl triggerOutputControl = new TriggerOutputControl(trigger, 1, DispatcherManager.create(DispatcherManagerHelper.CODE_PATH_DEFAULT_NAME))
@@ -124,6 +112,20 @@ abstract class ListenerSpecification extends Specification {
         threadPool = ControlledTriggerSvcThreadPool.getInstance()
         threadPool.max = 2
         threadPool.min = 0
+        // Create needed instances
+        ResponseMock filterResponseMock = new ResponseMock()
+        Service jsonRpcService = Mock(Service)
+        // Handle service layer communications
+        jsonRpcService.send(_ as Request, _ as Class) >> { Request request, Class c ->
+            println ">>> $request"
+            Response response = filterResponseMock.getResponse(request)
+            println "<<< $response"
+            response
+        }
+        // Inject mock service into Util
+        Util.instance.web3Service = Web3Service.build(jsonRpcService)
+        // Remember the contract address; implies the contract was deployed
+        Util.instance.storeContractAddress(pdt.getNSName(), filterResponseMock.contractAddress)
         // Run the listener
         listener = new DAppListenerMock(trigger, threadPool)
         threadPool.runTarget(listener)
